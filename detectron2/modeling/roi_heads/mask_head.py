8d2ce0929d6a189156df450c6ca6bbfea09c9cc8
Faruk Cankaya <farukcankayaaa@gmail.com>
2022-08-26 08:32:06 +0200

    feat: mask prediction images removed from tensorboard

--- detectron2/modeling/roi_heads/mask_head.py
+++ detectron2/modeling/roi_heads/mask_head.py
@@ -100,13 +100,14 @@ def mask_rcnn_loss(pred_mask_logits: torch.Tensor, instances: List[Instances], v
     storage.put_scalar("mask_rcnn/accuracy", mask_accuracy)
     storage.put_scalar("mask_rcnn/false_positive", false_positive)
     storage.put_scalar("mask_rcnn/false_negative", false_negative)
-    if vis_period > 0 and storage.iter % vis_period == 0:
-        pred_masks = pred_mask_logits.sigmoid()
-        vis_masks = torch.cat([pred_masks, gt_masks], axis=2)
-        name = "Left: mask prediction;   Right: mask GT"
-        for idx, vis_mask in enumerate(vis_masks):
-            vis_mask = torch.stack([vis_mask] * 3, axis=0)
-            storage.put_image(name + f" ({idx})", vis_mask)
+    # TODO: add back! removed to be faster
+    #if vis_period > 0 and storage.iter % vis_period == 0:
+    #    pred_masks = pred_mask_logits.sigmoid()
+    #    vis_masks = torch.cat([pred_masks, gt_masks], axis=2)
+    #    name = "Left: mask prediction;   Right: mask GT"
+    #    for idx, vis_mask in enumerate(vis_masks):
+    #        vis_mask = torch.stack([vis_mask] * 3, axis=0)
+    #        storage.put_image(name + f" ({idx})", vis_mask)
 
     mask_loss = F.binary_cross_entropy_with_logits(pred_mask_logits, gt_masks, reduction="mean")
     return mask_loss
